72cd0213271c92ee500dc462ceda8dd94c0be09f
Justin Z <contactjustinzamora@gmail.com>
2020-11-13 13:07:04 -0800

    added like functionality route

--- app.py
+++ app.py
@@ -244,6 +244,29 @@ def delete_user():
 
     return redirect("/signup")
 
+@app.route('/users/add_like/<int:message_id>', methods= ["GET, POST"])
+def add_like(message_id):
+    user = g.user.likes.query.get_or_404(id)
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    liked_message = Message.query.get_or_404(message_id)
+    if liked_message.user_id == g.user.id:
+        return abort(403)
+
+    user_likes = g.user.likes
+
+    if liked_message in user_likes: #if message is already liked by user then go else
+        g.user.likes = [like for like in user_likes if like != liked_message] #for every like that the user likes, return that like, if that like is not in (liked) message database
+    else: #if not: put it with our likes page
+        g.user.likes.append(liked_message)
+
+    db.session.commit()
+
+    return redirect("/")
+    return render_template('users/likes.html', user=user, likes=user.likes)
 
 ##############################################################################
 # Messages routes:
